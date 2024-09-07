451ef6110508950f8b7581887d7cff32150c4a7c
Maxime Haselbauer <renn0xtek9@laposte.net>
2024-09-07 13:35:28 +0200

    use imu data in fake imu (#5)
    * Use actual imu_data message in fake_imu
    
    * Create a communication protocols libraries to put some constant there
    
    * Refactor stdout of fake_imu

--- flight_software/libs/fake_imu/src/fake_imu.cpp
+++ flight_software/libs/fake_imu/src/fake_imu.cpp
@@ -1,7 +1,8 @@
 // Copyright 2024 <Maxime Haselbauer>
 #include <fake_imu/fake_imu.h>
-#include <fake_imu/software_in_the_loop_constant.h>
+#include <messages/imu_data.h>
 #include <os_abstraction_layer/os_abstraction_layer.h>
+#include <uart_imu/uart_imu.h>
 
 #include <array>
 #include <cstddef>
@@ -10,8 +11,23 @@
 #include <iostream>
 #include <thread>
 
+#include "utils.h"
+
 FakeImu::FakeImu(const OsAbstractionLayer::OsAbstractionLayerInterface& os_abstraction_layer)
     : os_layer_{os_abstraction_layer} {
+}
+
+messages::ImuData GenerateFakeImuData() {
+  messages::ImuData data{};
+  data.a_x = 0.1F;
+  data.a_y = 0.2F;
+  data.a_z = 0.3F;
+  data.omega_x = 0.4F;
+  data.omega_y = 0.5F;
+  data.omega_z = 0.5F;
+  data.temperature = 25.6F;
+  data.timestamp = std::chrono::nanoseconds{std::chrono::system_clock::now().time_since_epoch()};
+  return data;
 }
 
 void FakeImu::SimulateNormalOperation(const std::string& device_file_path,
@@ -19,8 +35,8 @@ void FakeImu::SimulateNormalOperation(const std::string& device_file_path,
   file_descriptor_ = os_layer_.OpenDeviceFile(device_file_path);
   const auto start_timestamp = std::chrono::high_resolution_clock::now();
   while ((file_descriptor_ > 0) && (std::chrono::high_resolution_clock::now() - start_timestamp < duration_ms)) {
-    SendImuData(ImuData{});
-    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME_BETWEEN_MEASUREMENTS_MS));
+    SendImuData(GenerateFakeImuData());
+    std::this_thread::sleep_for(uart_imu::SLEEP_TIME_BETWEEN_MESSAGES_US);
   }
   if (file_descriptor_ > 0) {
     os_layer_.CloseDeviceFile(file_descriptor_);
@@ -37,10 +53,11 @@ void FakeImu::SendByte(const std::byte& byte) {
     file_descriptor_ = -1;
     return;
   }
-  std::cout << "Sent byte: " << static_cast<int>(byte) << std::endl;
+  std::cout << byte << " ";
 }
 
-void FakeImu::SendImuData(const ImuData& data) {
+void FakeImu::SendImuData(const messages::ImuData& data) {
+  std::cout << std::endl << "Sending IMU data: " << data << std::endl;
   std::array<std::byte, sizeof(data)> payload{};
   std::memcpy(payload.data(), &data, payload.size());
   for (const auto& byte : payload) {
@@ -49,4 +66,5 @@ void FakeImu::SendImuData(const ImuData& data) {
     }
     SendByte(byte);
   }
+  std::cout << std::endl;
 }
---
+++ flight_software/system_interfaces/communication_protocols/include/uart_imu/uart_imu.h
@@ -0,0 +1,31 @@
+// Copyright 2024 <Maxime Haselbauer>
+#ifndef UART_IMU_UART_IMU_H
+#define UART_IMU_UART_IMU_H
+#include <cstddef>
+namespace uart_imu {
+constexpr std::size_t NUMBER_OF_BYTES_FOR_FLOAT_ENCODING{2U};
+constexpr std::size_t NUMBER_OF_BYTES_FOR_UART_COMMUNICATION{7U * NUMBER_OF_BYTES_FOR_FLOAT_ENCODING};
+
+constexpr float OUTPUT_DATA_RATE{208.0F};
+
+constexpr float LSB_SENSITIVITY_ACCELERATION{0.488E-03F};
+constexpr float LSB_SENSITIVITY_ANGULAR_VELOCITY{8.75E-3F};
+constexpr float LSB_SENSITIVITY_TEMPERATURE{1.0 / 256.0F};
+constexpr float TEMPERATURE_SENSOR_OFFSET{25.0F};
+
+constexpr int BAUDRATE{38400};
+constexpr std::chrono::microseconds BIT_DURATION_US{static_cast<int>(std::chrono::microseconds{1000000}.count()) /
+                                                    BAUDRATE};
+constexpr int NUMBER_START_BYTE{1};
+constexpr int NUMBER_OF_BITS_BY_FRAME{10};  // 1 start bit + 8 data bits + 1 stop bit
+constexpr int NUMBER_OF_BITS_BY_MESSAGE{NUMBER_OF_BITS_BY_FRAME *
+                                        (NUMBER_START_BYTE + NUMBER_OF_BYTES_FOR_UART_COMMUNICATION)};
+constexpr int REQUIRED_MINIMUM_BAUDRATE{static_cast<int>(OUTPUT_DATA_RATE) * NUMBER_OF_BITS_BY_MESSAGE};
+constexpr int NUMBER_OF_FREE_BIT_PER_SECOND{BAUDRATE - REQUIRED_MINIMUM_BAUDRATE};
+constexpr std::chrono::microseconds SLEEP_TIME_BETWEEN_MESSAGES_US{BIT_DURATION_US * NUMBER_OF_FREE_BIT_PER_SECOND};
+
+constexpr std::byte START_BYTE{0xAA};
+
+}  // namespace uart_imu
+
+#endif  // UART_IMU_UART_IMU_H
