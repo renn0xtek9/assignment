00ab394e52a4b3774f7d0d7afdd67379420eadcb
Maxime Haselbauer <renn0xtek9@laposte.net>
2024-09-08 18:31:11 +0200

    sync with start byte (#10)
    * Refactor the driver by polling faster until start byte is found
    
    * std::copy instead of std::memcpy when same type
    
    * Refactor and const what can be const

--- flight_software/libs/uart_imu_driver/src/driver.cpp
+++ flight_software/libs/uart_imu_driver/src/driver.cpp
@@ -7,29 +7,6 @@
 #include <algorithm>
 #include <thread>
 namespace uart_imu {
-Driver::Driver(const OsAbstractionLayer::OsAbstractionLayerInterface& os_abastrction_layer,
-               DriverContext& driver_context,
-               const std::string device_file_path)
-    : os_layer_{os_abastrction_layer}, driver_context_{driver_context}, device_file_path_{device_file_path} {
-}
-
-void Driver::Start() {
-  printf("Starting IMU driver.\n");
-  file_descriptor_ = os_layer_.OpenDeviceFile(device_file_path_);
-  if (file_descriptor_ < 0) {
-    std::cout << "Driver could not open device file: " << device_file_path_ << std::endl;
-    Stop();
-    return;
-  }
-  driver_thread_ = std::thread(&Driver::Run, this);
-}
-void Driver::Stop() {
-  printf("Stopping IMU driver.\n");
-  driver_must_stop = true;
-  if (driver_thread_.joinable()) {
-    driver_thread_.join();
-  }
-}
 
 bool IsThereAtLeastOneMessage(const std::vector<std::byte>& bytes_stream_from_imu) {
   return bytes_stream_from_imu.size() >= uart_imu::TOTAL_NUMBER_OF_BYTES;
@@ -49,43 +26,86 @@ void PushMesagesInQueue(std::vector<std::byte>& bytes_stream_from_imu,
                               bytes_stream_from_imu.begin() + uart_imu::TOTAL_NUMBER_OF_BYTES);
 }
 
-void FlushTheDeviceFile(const OsAbstractionLayer::OsAbstractionLayerInterface& os_layer, int file_descriptor) {
-  const int bytes_available = os_layer.ByteAvailableToRead(file_descriptor);
-  if (bytes_available > 0) {
-    std::vector<std::byte> byte_read_from_imu(bytes_available);
-    os_layer.ReadFromFile(file_descriptor, reinterpret_cast<char*>(byte_read_from_imu.data()), bytes_available);
+bool StartByteFound(const std::vector<std::byte> bytes_stream_from_imu) {
+  if (bytes_stream_from_imu.empty()) {
+    return false;
+  }
+  return bytes_stream_from_imu.front() == uart_imu::START_BYTE;
+}
+
+void CleanStreamUpToStartByte(std::vector<std::byte>& bytes_stream_from_imu) {
+  auto start_byte_iterator =
+      std::find(bytes_stream_from_imu.begin(), bytes_stream_from_imu.end(), uart_imu::START_BYTE);
+  bytes_stream_from_imu.erase(bytes_stream_from_imu.begin(), start_byte_iterator);
+}
+
+Driver::Driver(const OsAbstractionLayer::OsAbstractionLayerInterface& os_abastrction_layer,
+               DriverContext& driver_context,
+               const std::string device_file_path)
+    : os_layer_{os_abastrction_layer}, driver_context_{driver_context}, device_file_path_{device_file_path} {
+}
+
+void Driver::Start() {
+  printf("Starting IMU driver.\n");
+  file_descriptor_ = os_layer_.OpenDeviceFile(device_file_path_);
+  if (file_descriptor_ < 0) {
+    std::cout << "Driver could not open device file: " << device_file_path_ << std::endl;
+    Stop();
+    return;
+  }
+  driver_thread_ = std::thread(&Driver::Run, this);
+}
+void Driver::Stop() {
+  printf("Stopping IMU driver.\n");
+  driver_must_stop_ = true;
+  if (driver_thread_.joinable()) {
+    driver_thread_.join();
   }
 }
 
+void Driver::FlushTheDeviceFile() const {
+  ReadBytesFromDevice();
+}
+
+std::vector<std::byte> Driver::ReadBytesFromDevice() const {
+  const int bytes_available = os_layer_.ByteAvailableToRead(file_descriptor_);
+  if (bytes_available < 1) {
+    return {};
+  }
+  std::vector<std::byte> byte_read_from_imu(bytes_available);
+  os_layer_.ReadFromFile(file_descriptor_, reinterpret_cast<char*>(byte_read_from_imu.data()), bytes_available);
+  return byte_read_from_imu;
+}
+
 void Driver::Run() {
-  printf("IMU driver is running.\n");
   driver_context_.SetStatus(messages::ImuDriverStatus::OK);
-  FlushTheDeviceFile(os_layer_, file_descriptor_);
+  FlushTheDeviceFile();
 
   std::vector<std::byte> bytes_stream_from_imu{};
-  while (!driver_must_stop) {
-    int bytes_available = os_layer_.ByteAvailableToRead(file_descriptor_);
-    while (bytes_available < 1) {
-      std::this_thread::sleep_for(uart_imu::SLEEP_TIME_BETWEEN_MESSAGES_US);
-      bytes_available = os_layer_.ByteAvailableToRead(file_descriptor_);
-      if (driver_must_stop) {
+
+  while (!driver_must_stop_) {
+    while (!StartByteFound(bytes_stream_from_imu)) {
+      bytes_stream_from_imu = ReadBytesFromDevice();
+      CleanStreamUpToStartByte(bytes_stream_from_imu);
+      if (driver_must_stop_) {
         return;
       }
+      std::this_thread::sleep_for(uart_imu::FRAME_DURATION_US);
     }
     const auto timestamp = os_layer_.TimeStampNow();
 
-    std::vector<std::byte> byte_read_from_imu(bytes_available);
-    os_layer_.ReadFromFile(file_descriptor_, reinterpret_cast<char*>(byte_read_from_imu.data()), bytes_available);
+    // First bit has been received, now sleep until all the bytes are transferred.
+    std::this_thread::sleep_for(uart_imu::MESSAGE_DURATION_US);
+
+    auto byte_read_from_imu = ReadBytesFromDevice();
     bytes_stream_from_imu.insert(bytes_stream_from_imu.end(), byte_read_from_imu.begin(), byte_read_from_imu.end());
-    auto start_byte_iterator =
-        std::find(bytes_stream_from_imu.begin(), bytes_stream_from_imu.end(), uart_imu::START_BYTE);
-    bytes_stream_from_imu.erase(bytes_stream_from_imu.begin(), start_byte_iterator);
+    CleanStreamUpToStartByte(bytes_stream_from_imu);
     if (IsThereAtLeastOneMessage(bytes_stream_from_imu)) {
       PushMesagesInQueue(bytes_stream_from_imu, driver_context_, timestamp);
     }
+    // Message has been received and pushe. Now sleep until the start of next message.
     std::this_thread::sleep_for(uart_imu::SLEEP_TIME_BETWEEN_MESSAGES_US);
   }
 }
 
 }  // namespace uart_imu
-// Compare this snippet from flight_software/libs/uart_imu_driver/src/driver_context.cpp:
--- flight_software/libs/uart_imu_driver/tests/unit_tests.cpp
+++ flight_software/libs/uart_imu_driver/tests/unit_tests.cpp
@@ -64,7 +64,7 @@ TEST_F(UartImuDriverTest, UartImuFlushDeviceFileAtStart) {
 TEST_F(UartImuDriverTest, UartImuPollForByteavailableAtStart) {
   EXPECT_CALL(os_abstraction_layer_, OpenDeviceFile("some_device_file")).WillOnce(Return(42));
   EXPECT_CALL(os_abstraction_layer_, ByteAvailableToRead(42)).WillOnce(Return(0)).WillRepeatedly(Return(1));
-  EXPECT_CALL(os_abstraction_layer_, TimeStampNow()).Times(AtLeast(1));
+  // EXPECT_CALL(os_abstraction_layer_, TimeStampNow()).Times(AtLeast(1));
   EXPECT_CALL(os_abstraction_layer_, ReadFromFile(42, _, 1)).Times(AtLeast(1));
 
   uart_imu::Driver driver(os_abstraction_layer_, imu_driver_context_, {"some_device_file"});
@@ -100,7 +100,8 @@ TEST_F(UartImuDriverTestReadingFile, UartImuReadBytesFromFile) {
       .WillOnce(Return(0))
       .WillRepeatedly(Return(static_cast<int>(uart_imu::TOTAL_NUMBER_OF_BYTES)));
   EXPECT_CALL(os_abstraction_layer_, TimeStampNow()).Times(AtLeast(1)).WillRepeatedly(Return(expected_timestamp));
-  EXPECT_CALL(os_abstraction_layer_, ReadFromFile(42, _, static_cast<int>(uart_imu::TOTAL_NUMBER_OF_BYTES)));
+  EXPECT_CALL(os_abstraction_layer_, ReadFromFile(42, _, static_cast<int>(uart_imu::TOTAL_NUMBER_OF_BYTES)))
+      .Times(AtLeast(1));
 
   os_abstraction_layer_.SetBytesToReturn(AssembleFrame(serializer::uart::Serialize(expected_imu_data)));
 
